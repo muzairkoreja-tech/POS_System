c696b878874268c2527ecda69f7cffeaaa5f19ef
muzairkoreja-tech <m.uzairkoreja@gmail.com>
2026-05-13 02:08:53 +0500

    Fix Vercel deployment config

--- api/index.py
+++ api/index.py
@@ -3,10 +3,8 @@ import os
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from serverless_wsgi import handle_request
-from app import app, init_db
-
-init_db()
+from serverless_wsgi import handle_request_wsgi
+from app import app
 
 def handler(event, context):
-    return handle_request(app, event, context)
+    return handle_request_wsgi(app, event, context)
